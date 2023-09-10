1add27c6455ab86bb36fb83dc22716c4b7f6f81b
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-09-10 00:17:53 -0600

    agregue iconos

---
+++ src/plugins/oh-vue-icon.ts
@@ -0,0 +1,19 @@
+
+import { addIcons, OhVueIcon} from "oh-vue-icons";
+
+import{
+    IoAddCircle,
+    FaUserAlt,
+    RiLogoutBoxLine,
+    FaCriticalRole
+} from "oh-vue-icons/icons";
+
+
+
+addIcons(
+    IoAddCircle,
+    FaUserAlt,
+    RiLogoutBoxLine,
+    FaCriticalRole
+);
+export default OhVueIcon;
