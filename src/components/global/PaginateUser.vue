f50e3d0b14542b6035108aaeb00185bc583f5139
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-09-11 22:20:05 -0600

    solucion de errores al coponente global y cambios al estilo

--- src/components/global/PaginateUser.vue
+++ src/components/global/PaginateUser.vue
@@ -1,56 +1,57 @@
 <template>
-    <ul class="flex w-9 justify-center">
-      <li>
-        <button
-          @click="changePage(prev)"
-          class="mx-1 bg-blue-950 cursor-pointer rounded-full p-2 w-12 h-12 text-center text-lg text-white"
-          :disabled="currentPage === 1"
-        >
-          <oh-vue-icon name="bi-arrow-left" scale="1.5"></oh-vue-icon>
-        </button>
-      </li>
-      <li
-        v-for="page in pages"
-        :key="page"
-        :class="['mx-1 border font-semibold border-blue rounded-full p-2 w-12 h-12 text-center text-sm flex justify-center items-center cursor-pointer', currentPage === page ? 'bg-blue-958 text-white' : 'text-blue-956']"
-        @click="changePage(page)"
+  <ul class="m justify-center w-full h-[-30% ]  
+         left-[18%]  flex ">
+    <li>
+      <button
+        @click="changePage(prev)"
+        class="mx-1 bg-white border border-blue cursor-pointer rounded-full p-2 w-12 h-12 text-center text-lg text-black"
+        :disabled="currentPage === 1"
       >
-        {{ page }}
-      </li>
-      <li>
-        <button
-          class="mx-1 bg-blue-950 rounded-full p-2 w-12 h-12 cursor-pointer text-center text-lg text-white"
-          :disabled="currentPage === totalPag"
-          @click="changePage(next)"
-        >
-          <!-- <oh-vue-icon name="OiStarFill" scale="1.5"></oh-vue-icon> -->
-        </button>
-      </li>
-    </ul>
-  </template>
-  
+      <oh-vue-icon name="bi-arrow-left-circle-fill" scale="1.5"/> 
+      </button>
+    </li>
+    <li
+      v-for="page in pages"
+      :key="page"
+      :class="['mx-1 border font-semibold border-blue rounded-full p-2 w-12 h-12 text-center text-sm text-black flex  h[calc(100% -20%)] left-[19.5%] justify-center items-center cursor-pointer', currentPage === page ? 'bg-blue-950 text-white' : 'text-blur-950']"
+      @click="changePage(page)"
+    >
+      {{ page }}
+    </li>
+    <li>
+      <button
+        class="mx-1 border border-blue rounded-full p-2 w-12 h-12 cursor-pointer text-center text-lg text-black"
+        :disabled="currentPage === totalPag"
+        @click="changePage(next)"
+      >
+      <oh-vue-icon name="bi-arrow-right-circle-fill" scale="1.5" /> 
+      </button>
+    </li>
+  </ul>
+</template>
+
 
 <script lang="ts" setup>
-// import  OhVueIcon from "../../plugins/oh-vue-icons"
+import OhVueIcon from "../../plugins/oh-vue-icon"
 
 interface Props {
-  pages: Array<number | string>;
-    currentPage: number;
-    totalPag: number;
-    next: number;
-    prev: number;
+pages: Array<number | string>;
+  currentPage: number;
+  totalPag: number;
+  next: number;
+  prev: number;
 }
 
 const { pages, currentPage, totalPag, next, prev } = defineProps<Props>();
 
 type Emits = {
-    (event: "method", page: number | string): void;
+  (event: "method", page: number | string): void;
 }
 
 const emits = defineEmits<Emits>();
 
 const changePage = (page: number | string) => {
-    emits("method", page)
+  emits("method", page)
 }
 
 
