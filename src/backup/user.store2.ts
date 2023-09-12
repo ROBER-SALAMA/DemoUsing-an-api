bed5e2de5a3145f2a38d80f42600b15e4613e556
Roberto Salama <102178835+ROBER-SALAMA@users.noreply.github.com>
2023-09-11 21:34:20 -0600

    cambios

--- src/backup/user.store2.ts
+++ src/backup/user.store2.ts
@@ -1,95 +1,108 @@
-import {defineStore} from "pinia";
-import { get_user, create_user,update_user,delete_user, get_paginate_users } from "../services/user.service";
-import { CreateUser, GetUser,UpdateUser } from "../types/user.type";
-import {useToast} from"vue-toastification";
-import {paginate} from "../algorith/paginated";
+import { defineStore } from "pinia"
+import { get_user, create_user, update_user, delete_user, get_paginate_users } from "../services/user.service";
+import { CreateUser, GetUser, UpdateUser } from "../types/user.type";
+import { useToast } from "vue-toastification";
+import { paginate } from "../algorith/paginated"
 
 const toast = useToast();
-export const UseUserStore = defineStore("user",{
-    state:() => ({
-        user:[] as GetUser[],
-        pages:[] as Array<string | number>,
-        paginates:{
-            total:0,
-            totalPages:0,
-            nextPag:0,
-            prevPag:0,
-            currentPage:0,
+export const UseUserStore = defineStore("user", {
+    state: () => ({
+        user: [] as GetUser[],
+        pages: [] as Array<string | number>,
+        paginates: {
+            total: 0,
+            totalPages: 0,
+            nextPag: 0,
+            prevPag: 0,
+            currentPage: 0,
         },
     }),
-    actions:{
+    actions: {
         async GetAllUser(){
             try {
                 const data = await get_user();
-                this.user = data.users
-            }catch{
-                toast.error("Error");
+                this.user = data.users;
+            } catch{
+                toast.error("Error")
             }
         },
-        async GetUsers(page:number, take:number){
+
+        //! pagenation
+        async GetUsers(page: number, take: number) {
             try {
-                const data = await get_paginate_users(page,take);
-                this.user = data.users
-                this.paginates ={
+                const data = await get_paginate_users(page, take);
+                this.user = data.users;
+                this.paginates = {
                     total: data.total,
                     totalPages: data.totalPages,
                     nextPag: data.nextPage,
                     prevPag: data.prevPage,
-                    currentPage: page,
+                    currentPage: data.currentPage,
                 };
-                this.pages = paginate(page,data.totalPages);
-            } catch  {
-               toast.error("error de servidor"); 
+                this.pages = paginate(page, data.total)
+            } catch {
+                toast.error("Error de servidor")
             }
         },
 
-        async CreateUser(values: CreateUser){
+        async CreateUser(values: CreateUser) {
             try {
                 const data = await create_user(values);
+
                 if (data.ok) {
-                    toast.success("usuario creado");
                     await this.GetAllUser()
-                    await this.GetUsers (this.paginates.currentPage, 5);
-                } else {
-                   toast.error("Error al crear el usuario"); 
+
+                    //! pagenation
+                    await this.GetUsers(this.paginates.currentPage, 5);
+                    toast.success("Usuario creado");
+                }else{
+                    toast.error("Error al crear el usuario")
                 }
             } catch (error) {
                 toast.error("Error en el servidor");
+            }
+        },
+
+        async DeleteUser(id: number) {
+            try {
+                const data = await delete_user(id);
+                if (data.ok) {
+                    await this.GetAllUser()
+                    toast.success("Usuario eliminado")
+
+                    //! pagenation
+                    const currentPage = this.paginates.currentPage;
+                    const totalUsers = this.user.length;
+
+                    if (currentPage > 1 && totalUsers === 1) {
+                        await this.GetUsers(currentPage - 1, 5);
+                    } else {
+                        await this.GetUsers(currentPage, 5);
+                    }
+                }
+            } catch {
+                toast.error("Error")
             }
         },
 
         async UpdateUser(id:number, values:UpdateUser) {
             try {
-               const data = await update_user(id, values);
-               if (data.ok) {
-                    toast.info("Usuario actualizado");
-                    await this.GetAllUser()
-                    await this.GetUsers(this.paginates.currentPage,5)
-               } else {
-                toast.error("Error al actualizar usuario")
-               } 
-            } catch (error) {
-                toast.error("Error en el servidor");
-            }
-        },
+                const data = await update_user(id, values);
 
-        async DeleteUser(id:number){
-            try {
-                const data = await delete_user(id);
                 if (data.ok) {
-                    const currentPage = this.paginates.currentPage;
-                    const totalUsers = this.user.length;
-                    toast.success("Usuario eliminado");
+                    toast.info("Usuario actualizado")
                     await this.GetAllUser()
-                    if (currentPage >1 && totalUsers ===1) {
-                        await this.GetUsers(currentPage -1, 5);
-                    } else{
-                        await this.GetUsers(currentPage,5)
-                    }
+
+                    //! pagenation
+                    await this.GetUsers(this.paginates.currentPage, 5);
+                }else{
+                    toast.error("Error al actualizar usuario")
                 }
-            } catch  {
-                toast.error("Error");
+
+            } catch (error) {
+                toast.error("Error en el servidor")
             }
-        },
-    },
-});
+        }
+    }
+    
+})
